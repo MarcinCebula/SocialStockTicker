b7c80509ed23be7dc8d0aadca726dee16163ef05
Marcin K Cebula <MarcinRKL@gmail.com>
2012-06-14 15:19:47 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ SocialStockTicker::Application.configure do
   config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false``
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
